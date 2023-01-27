18992054d472f7259f164f27af26b4ca3c69994d
gadmel <abramov.gleb@gmail.com>
2023-01-27 15:10:43 +0100

    Create unworking dummies.

---
+++ src/main/java/Expert.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+
+public class Expert {
+	public static String backwardsString() {
+
+	};
+	public static Array accendingArray(Array incoming) {
+
+	};
+
+	public static Array backwardsString( String incoming) {
+
+	};
+
+	public int sumOfIntDigits(int incoming) {
+
+	};
+
+	public int transformToRoman(int incoming) {
+
+	};
+
+}
