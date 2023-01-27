ccc636567bb0e3c42803604499f36308c039bc65
gadmel <abramov.gleb@gmail.com>
2023-01-27 15:18:17 +0100

    Create unworking test dummies.

---
+++ src/test/java/ExpertTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpertTest {
+
+	@Test
+	void backwardsString() {
+	}
+
+	@Test
+	void accendingArray() {
+	}
+
+	@Test
+	void testBackwardsString() {
+	}
+
+	@Test
+	void sumOfIntDigits() {
+	}
+
+	@Test
+	void transformToRoman() {
+	}
+}
